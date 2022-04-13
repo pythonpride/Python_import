ec089b16e7d1a21d206925285003d86cd7fe930d
Oxana Lezhneva <pythonpride@ya.ru>
2022-04-13 11:11:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from application import salary
 from application.db import people
+import datetime
 
 if __name__ == '__main__':
+    now = datetime.datetime.now()
+    print(now.strftime("%d-%m-%Y %H:%M"))
     people.get_employees()
     salary.calculate_salary()
